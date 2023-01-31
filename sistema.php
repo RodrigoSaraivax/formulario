b9c29c636629dd996e49e0aa4b81df3b1adece65
Rodrigo Saraiva <rodrigosa123@gmail.com>
2023-01-31 19:12:12 -0300

    Pagina de sistema listando todos os dados do banco.

--- sistema.php
+++ sistema.php
@@ -1,17 +1,24 @@
 <?php
-    session_start();
-    /* print_r($_SESSION); */
-    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
-    {
-        unset($_SESSION['email']);
-        unset($_SESSION['senha']);
-        header('Location:login.php');
-    }
-    $logado = $_SESSION['email'];
+session_start();
+include_once('config.php');
+/* print_r($_SESSION); */
+if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
+    unset($_SESSION['email']);
+    unset($_SESSION['senha']);
+    header('Location:login.php');
+}
+$logado = $_SESSION['email'];
+
+$sql = "SELECT * FROM usuarios";
+
+$sqlResult = $conexao->query($sql);
+
+/* print_r($sqlResult); */
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -27,25 +34,72 @@
             color: white
         }
 
-        .header{
+        .header {
             background-color: dodgerblue;
             height: 40px;
         }
 
-        p{
+        p {
             text-align: left;
+        }
+
+        .table_bg {
+            background-color: rgba(0,0,0,0.3);
+            border-radius: 15px,15px,0,0;
         }
     </style>
 </head>
+
 <body>
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Sistema</a>
-    <a href="sair.php" class="btn btn-danger me-3">Sair</a>
-  </div>
-</nav>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Sistema</a>
+            <a href="sair.php" class="btn btn-danger me-3">Sair</a>
+        </div>
+    </nav>
 
     <h1>Acessou o Sistema!</h1>
-    <h2>Olá <?=$logado?>!</h2>
+    <h2>Olá <?= $logado ?>!</h2>
+
+    <div class="m-5">
+        <table class="table text-white table_bg">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Senha</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Gênero</th>
+                    <th scope="col">Data de nascimento</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">Endereço</th>
+                    <th scope="col">...</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while($user_data = mysqli_fetch_assoc($sqlResult)){
+                        echo "<tr>";
+                        echo "<td>" . $user_data['id'] . "</td>";
+                        echo "<td>" . $user_data['nome'] . "</td>";
+                        echo "<td>" . $user_data['senha'] . "</td>";
+                        echo "<td>" . $user_data['email'] . "</td>";
+                        echo "<td>" . $user_data['telefone'] . "</td>";
+                        echo "<td>" . $user_data['genero'] . "</td>";
+                        echo "<td>" . $user_data['data_nascimento'] . "</td>";
+                        echo "<td>" . $user_data['cidade'] . "</td>";
+                        echo "<td>" . $user_data['estado'] . "</td>";
+                        echo "<td>" . $user_data['endereco'] . "</td>";
+                        echo "<td>" . "Ações" . "</td>";
+                        echo "</tr>";
+                    }
+
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>
